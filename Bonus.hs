5a8426d25b6e345ef5ecb3f2a126ab28c9f79948
Chris Frisz <cjfrisz@chrisfrisz.com>
2012-01-09 12:26:33 -0500

    Adding main file Bonus.hs.

---
+++ Bonus.hs
@@ -0,0 +1,12 @@
+{-
+---------------------------------------------------------------------
+- File Bonus.hs
+- Written by Chris Frisz
+- 
+- Created  9 Jan 2012
+- Last modified  9 Jan 2012
+- 
+- The file Bonus.hs is the main file for the Bonus Round application.
+---------------------------------------------------------------------
+-}
+
